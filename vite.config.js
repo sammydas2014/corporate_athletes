5e53a7ddd2e4963333ebed2aad25ea4a53ab8578
Sammy das <sammydas2014@gmail.com>
2026-06-01 14:52:32 +0530

    error solving

--- vite.config.js
+++ vite.config.js
@@ -5,14 +5,16 @@ import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({ command }) => {
+  return {
+    plugins: [
+      vue(),
+      command === 'serve' ? vueDevTools() : null,
+    ].filter(Boolean),
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      },
     },
-  },
+  }
 })
